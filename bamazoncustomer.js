7698f893f25c1c3fd3fa084797382e853122de59
ejpelagio <ejpelagio@yahoo.com>
2016-12-06 09:03:01 -0600

    added files and json

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -3,6 +3,7 @@ var mysql = require('mysql');
 var prompt = require('prompt');
 var colors = require('colors/safe');
 var Table = require('cli-table');
+var inquirer = require("inquirer");
 // Path to my local host to connect with MySql
 var connection = mysql.createConnection({
 	  host     : '127.0.0.1',
@@ -19,14 +20,3 @@ connection.connect();
 //connect to the mysql database and pull the information from the Products database to display to the user
 connection.query('SELECT product_id, product_name, department_name, price, stock_quantity', function(err, result){
 	if(err) console.log(err);
-
-	//creates a table for the information from the mysql database to be placed
-	var table = new Table({
-		head: ['product_id', 'product_name', 'price'],
-		style: {
-			head: ['blue'],
-			compact: false,
-			colAligns: ['center'],
-		}
-	
- 
