5d7b92dd3163604592a4fc153926bba21a8232b0
ejpelagio <ejpelagio@yahoo.com>
2016-12-06 23:43:14 -0600

    initian manager

---
+++ bamazonmanager.js
@@ -0,0 +1,21 @@
+// Install and require this NPM
+var mysql = require('mysql');
+var prompt = require('prompt');
+var colors = require('colors/safe');
+var Table = require('cli-table2');
+var inquirer = require("inquirer");
+// Path to my local host to connect with MySql
+var connection = mysql.createConnection({
+	  host     : '127.0.0.1',
+	  port	   : 3306, 
+	  user     : "root",
+	  password : 'root',
+	  database : 'Bamazon_db'
+});
+
+//Creating initial connection to database using MYSQL NPM packages.
+connection.connect(function(err) {
+	if (err) throw err;
+ 	 //console.log("Conection test");
+});
+
